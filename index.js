d6b6399930dae785e282b4e9a9072375bca8c9d1
Arley Mauricio Duarte <arleymauricio@gmail.com>
2023-06-01 11:08:11 -0400

    improvement commit to my Integrating With HubSpot I: Foundations practicum repository.

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', async (req, res) => {
         const resp = await axios.get(pets, { headers });
         const data = resp.data.results;
         console.log(data);
-        res.render('pets', { title: 'Pets | HubSpot APIs', addUrl: addUrl, data });       
+        res.render('homepage', { title: 'Pets | HubSpot APIs', addUrl: addUrl, data });       
     } catch (error) {
         console.error(error);
     }
